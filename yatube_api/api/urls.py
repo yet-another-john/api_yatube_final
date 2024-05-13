4c5c3c70fe494e4429135843084463fb0cac444b
Evgenii Stoliarov <stoliarovea@gmail.com>
2024-05-13 20:50:21 +0300

    13.05.2024

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,23 +1,18 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from . import views
-from .views import FollowViewSet, PostViewSet
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet, basename='posts')
-router.register(r'follow', FollowViewSet, basename='follow')
+router.register(r'posts', PostViewSet, basename="posts")
+router.register(r'follow', FollowViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'^posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename="comments")
+
 
 urlpatterns = [
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
     path('v1/', include(router.urls)),
-    path('v1/posts/<int:pk>/', views.api_posts_detail),
-    path('v1/groups/', views.api_groups),
-    path('v1/groups/<int:pk>/', views.api_groups_detail),
-    path('v1/posts/<int:pk>/comments/', views.api_posts_detail_comments),
-    path(
-        'v1/posts/<int:post_pk>/comments/<int:comment_pk>/',
-        views.api_posts_detail_comments_detail
-    ),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Comment, Follow, Group, Post
-from rest_framework import filters, status, viewsets
-from rest_framework.decorators import api_view
+from posts.models import Follow, Group, Post
+from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
-from rest_framework.response import Response
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
@@ -22,75 +20,23 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def api_posts_detail(request, pk):
-    post = get_object_or_404(Post, id=pk)
-    if request.method == 'GET':
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-    if request.method == 'PUT' or request.method == 'PATCH':
-        if request.user != post.author:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        serializer = PostSerializer(post, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        if request.user != post.author:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
 
-@api_view(['GET'])
-def api_groups(request):
-    groups = Group.objects.all()
-    serializer = GroupSerializer(groups, many=True)
-    return Response(serializer.data)
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly]
 
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
 
-@api_view(['GET'])
-def api_groups_detail(request, pk):
-    group = get_object_or_404(Group, id=pk)
-    serializer = GroupSerializer(group)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'POST'])
-def api_posts_detail_comments(request, pk):
-    if request.method == 'POST':
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user, post_id=pk)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    comments = Comment.objects.filter(post_id=pk)
-    serializer = CommentSerializer(comments, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def api_posts_detail_comments_detail(request, post_pk, comment_pk):
-    comment = get_object_or_404(Comment, id=comment_pk)
-    if request.method == 'GET':
-        serializer = CommentSerializer(comment)
-        return Response(serializer.data)
-    if request.method == 'PUT' or request.method == 'PATCH':
-        if request.user != comment.author:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        serializer = CommentSerializer(
-            comment, data=request.data, partial=True
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        if request.user != comment.author:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        comment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_queryset(self):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        return post.comments.all()
 
 
 class FollowViewSet(viewsets.ModelViewSet):
